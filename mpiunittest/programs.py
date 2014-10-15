5248ba3b76bffc4a4d814aa8496a126a757ee37a
g <SimplyKnownAsG@users.noreply.github.com>
2014-10-14 21:17:01 -0700

    removed some commented out code...

--- mpiunittest/programs.py
+++ mpiunittest/programs.py
@@ -1,13 +1,13 @@
 import sys
-from unittest.main import TestProgram
+from unittest import main as TestProgram
 from unittest import loader
 
-# import mpiunittest as mut
 from mpiunittest import actions
 from mpiunittest import suites
 
 class SerialTestProgram(TestProgram):
   pass
+
 
 class MasterTestProgram(TestProgram):
 
@@ -16,12 +16,6 @@ class MasterTestProgram(TestProgram):
     kwargs['exit'] = False
     TestProgram.__init__(self, **kwargs)
 
-#   def runTests(self):
-#     TestProgram.runTests(self)
-#     quit = actions.StopAction()
-#     for rank in range(1, mut.SIZE):
-#       mut.COMM_WORLD.send(quit, dest=rank)
-
 
 class WorkerTestProgram(TestProgram):
 
@@ -29,10 +23,4 @@ class WorkerTestProgram(TestProgram):
     loader.TestLoader.suiteClass = suites.WorkerTestSuite
     kwargs['exit'] = False
     TestProgram.__init__(self, **kwargs)
-
-#   def runTests(self):
-#     not_done = True
-#     while not_done:
-#       action = mut.COMM_WORLD.recv(None, source=0)
-#       not_done = action.invoke()
 
