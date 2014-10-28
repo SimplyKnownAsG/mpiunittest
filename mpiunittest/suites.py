14789b69e7fafad536f5e70620dedc38f485d6e4
g <SimplyKnownAsG@users.noreply.github.com>
2014-10-28 07:05:28 -0700

    Implemented asynchronous stuff... easier than I would have thought.

--- mpiunittest/suites.py
+++ mpiunittest/suites.py
@@ -35,25 +35,41 @@ class RunSuiteAction(actions.Action):
 
 
 class MasterTestSuite(SerialTestSuite):
+  
+  # def __init__(self, *args, **kwargs):
+  #   SerialTestSuite.__init__(self, *args, **kwargs)
+  #   self._mpi_requests = {}
+  #   for rank in range(1, mut.SIZE - 1):
+  #     self._replace_request(rank)
 
   def run(self, result, debug=False):
     self._result = result
     suites = self._flatten()
-    print('Found {} suites, will distribute across {} processors.'
-          .format(len(suites), mut.SIZE - 1))
+    result.stream.writeln('Found {} suites, will distribute across {} processors.'
+                          .format(len(suites), mut.SIZE - 1))
     for suite in suites:
       actions.RequestWorkAction.add_work(RunSuiteAction(suite))
     waiting = [True] + [False for _ in range(1, mut.SIZE)]
     while actions.RequestWorkAction._backlog or not all(waiting):
       for rank in range(1, mut.SIZE):
-        workRequest = mut.COMM_WORLD.recv(None, source=rank)
-        if not isinstance(workRequest, actions.Action):
-          raise actions.MpiActionError(workRequest)
-        workRequest.invoke()
-        waiting[rank] = isinstance(workRequest, actions.RequestWorkAction)
+        if not mut.COMM_WORLD.Iprobe(source=rank):
+          continue
+        mpi_action = mut.COMM_WORLD.recv(source=rank)
+        if not isinstance(mpi_action, actions.Action):
+          raise actions.MpiActionError(mpi_action)
+        mpi_action.invoke()
+        waiting[rank] = isinstance(mpi_action, actions.RequestWorkAction)
     for _ in range(1, mut.SIZE):
       mut.COMM_WORLD.send(actions.StopAction(), dest=_)
     return result
+  
+  def _replace_request(self, rank):
+    mpi_req = self._mpi_requests.get(rank, None)
+    if mpi_req is not None:
+      mpi_req.Cancel()
+      mpi_req.Free()
+    self._mpi_requests[rank] = mut.COMM_WORLD.irecv(None, dest=rank)
+    print('replacing {}.. {}'.format(rank, self._mpi_requests[rank]))
   
   def _flatten(self):
     suites = []
