ac72c7b0394e434ae1daa6de54ff583b192884d8
g <SimplyKnownAsG@users.noreply.github.com>
2014-12-08 18:10:18 -0800

    rearranged some stuff in the logger, apparently .send must have .recv, which the opposite is also true, making a .send and .recv on the same rank impossible. Also, since an isend can happen quickly, it appears that the logger loop will get the correct number of .isent objects, but only receiv the latest (so, if i isend 'hi', 'hello', 'bye', really quickly, 'bye' will print 3 times). The logger thread now has a list for backlog from the same process, it doesn't have a lock to make sure the array is properly appended, so it could be better.

--- mut/logger.py
+++ mut/logger.py
@@ -22,11 +22,7 @@ def start_log_thread():
 
 
 def stop_log_thread():
-    mut.COMM_WORLD.Barrier()
-    if mut.RANK == MUT_LOG_PROCESSOR:
-        mut.COMM_WORLD.isend(actions.StopAction(), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
-        while _log_thread.is_alive():
-            print('waiting for log thread to die')
+    mut.COMM_WORLD.isend(actions.StopAction(), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
 
 
 def _format_message(message):
@@ -40,49 +36,65 @@ def _format_message(message):
 def log(message):
     msg = _format_message(message)
     if msg is not None:
-        mut.COMM_WORLD.isend(LogMessageAction(msg), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
+        msg_action = LogMessageAction(msg)
+        if mut.RANK == MUT_LOG_PROCESSOR:
+            _log_thread.add_backlog(msg_action)
+        else:
+            mut.COMM_WORLD.send(msg_action, dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
 
 
 def write(message):
     if isinstance(message, basestring) and len(message) > 0:
-        mut.COMM_WORLD.isend(LogMessageAction(message), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
+        mut.COMM_WORLD.send(LogMessageAction(message), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
 
 def all_log(message):
-    messages = mut.COMM_WORLD.gather(_format_message(message + '\n'), root=MUT_LOG_PROCESSOR)
+    messages = mut.COMM_WORLD.gather(_format_message(message), root=MUT_LOG_PROCESSOR)
     if mut.RANK == MUT_LOG_PROCESSOR:
         msg = ''.join(messages)
-        mut.COMM_WORLD.isend(LogMessageAction(msg), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
+        mut.COMM_WORLD.send(LogMessageAction(msg), dest=MUT_LOG_PROCESSOR, tag=_MUT_LOG_TAG)
 
 
 class LogThread(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._local_backlog = []
+
+    def add_backlog(self, message_action):
+        self._local_backlog.append(message_action)
+
+    def dump_backlog(self):
+        backlog = self._local_backlog
+        self._local_backlog = []
+        for message_action in backlog:
+            message_action.invoke()
 
     def run(self):
         _stderr.write(_format_message('starting logging thread.'))
+        not_done = [True for _ in range(mut.SIZE)]
         try:
-            not_done = True
-            while not_done:
+            while any(not_done):
                 for rank in range(mut.SIZE):
+                    if rank == MUT_LOG_PROCESSOR:
+                        self.dump_backlog()
                     if not mut.COMM_WORLD.Iprobe(source=rank, tag=_MUT_LOG_TAG):
                         continue
                     mpi_action = mut.COMM_WORLD.recv(source=rank, tag=_MUT_LOG_TAG)
                     if not isinstance(mpi_action, actions.Action):
                         raise actions.MpiActionError(mpi_action)
-                    not_done = mpi_action.invoke()
-                    if not not_done:
-                        break
-        except Exception as shit:
+                    not_done[rank] = mpi_action.invoke()
+        except Exception as err:
             _stderr.write(_format_message('Something bad happened while receiving message from {}.'
                                           .format(rank)))
-            for cc in shit.message:
-                _stderr.write(_format_message('{} == {}'.format(cc, ord(cc))))
-            _stderr.write(_format_message('Exception: {}\n'.format(shit)))
+            _stderr.write(_format_message('Exception: {}\n'.format(err)))
             stack = traceback.format_exc()
             _stderr.write(_format_message(stack))
-            mut.COMM_WORLD.Abort(-1)
+            #mut.COMM_WORLD.Abort(-1)
         _stderr.write(_format_message('stopping logging thread.\n'))
 
 
 class LogMessageAction(actions.Action):
+
     def __init__(self, message):
         self._message = message
 
@@ -93,12 +105,11 @@ class LogMessageAction(actions.Action):
 
 if __name__ == '__main__':
     try:
-        _stdout.write(_format_message('about to start thread'))
         start_log_thread()
-        _stdout.write(_format_message('about to log message'))
         log('howdy!')
-        _stdout.write(_format_message('just logged message'))
+        log('hi!')
+        log('hello!')
+        log('goodbye!')
     finally:
-        _stdout.write(_format_message('stopping the log thread'))
         stop_log_thread()
 
